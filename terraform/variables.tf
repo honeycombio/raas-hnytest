0e706b4578a52d2570ae2c2544e3c56c9ebd554a
sgsharma <sashasharma@honeycomb.io>
2025-07-21 18:28:19 -0700

    update to align with recent changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "slack_recipient_channel" {
   type    = string
   default = "#collab-hosted-refinery-for-pocs"
 }
+
+variable "create_slack_recipient" {
+  type    = bool
+  default = false
+}
